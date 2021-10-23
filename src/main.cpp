dccc22039d36f8b401ece2fc2800e35fb7312371
Cameron Stewart <cstewart000@gmail.com>
2021-10-23 16:43:14 +1300

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,39 @@
 
 #include <Arduino.h>
 #include <Wire.h>
-#include <Button.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <ezButton.h>
-
+#include <Switch.h>
 
 
 
 //HARDWARE PINOUT
 
-//override switches
-#define PIN_TOGGLE_1 2
-#define PIN_TOGGLE_2 3
-#define PIN_TOGGLE_3 4
-#define PIN_TOGGLE_MODE 6 //NOT YET USED.
-
 //gates
-#define PIN_GATE_1 =6
-#define PIN_GATE_2 =7
-#define PIN_GATE_3 =8
+
+#define PIN_OVERRIDE_blast_gate_1_button 2
+#define PIN_OVERRIDE_blast_gate_2_button 3
+#define PIN_OVERRIDE_blast_gate_3_button 4
+
+#define PIN_BLAST_blast_gate_1_button 6
+#define PIN_BLAST_blast_gate_2_button 7
+#define PIN_BLAST_blast_gate_3_button 8
 
 
-//#define PIN_GATE_1
+
+//#define
 
 #define PIN_MODE_SWITCH 12
-
 #define PIN_ALERT_LED 13
+#define PIN_PCA9865_ENABLE 10
 
-
-#define GATE_1 1
-#define GATE_2 2
-#define GATE_3 3
+#define BUTTERFLY_GATE_1 1
+#define BUTTERFLY_GATE_2 2
+#define BUTTERFLY_GATE_3 3
 
 #define NUM_GATES 3
 
 
-//SERVO CALIBRATION
+//SERVO CALIBRATION - See adafruit website
 #define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
 #define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
@@ -46,15 +43,13 @@
 //OBJECT INSTANTIATION
 Adafruit_PWMServoDriver servo_driver = Adafruit_PWMServoDriver();
 
-ezButton override1button(2);
-ezButton override2button(3);
-ezButton override3button(4);
+Switch override_gate_1_button(PIN_OVERRIDE_blast_gate_1_button);
+Switch override_gate_2_button(PIN_OVERRIDE_blast_gate_2_button);
+Switch override_gate_3_button(PIN_OVERRIDE_blast_gate_3_button);
 
-Button gate_1(6);
-Button gate_2(7);
-Button gate_3(8);
-Button mode(12);
-
+Switch blast_gate_1_button(PIN_BLAST_blast_gate_1_button);
+Switch blast_gate_2_button(PIN_BLAST_blast_gate_2_button);
+Switch blast_gate_3_button(PIN_BLAST_blast_gate_3_button);
 
 //FUNCTION declarations
 void setDustExtractor(bool state);
@@ -62,41 +57,31 @@ void setGateConfig(int gate);
 void controlGate(int gateNumber, bool state);
 void printSystemState();
 bool areAnyGatesOpen();
-bool closeAllGates();
+void closeAllGates();
 void checkOverideButtons();
 void controlGatesByArray();
 void checkGateSwitchState();
-
 
 //State variables
 bool extractorState = false;
 int gate_config_state = 0;
 bool gate_states_array[NUM_GATES];
-
+bool previous_gate_states_array[NUM_GATES];
+bool button_states_array[NUM_GATES];
+bool previous_button_states_array[NUM_GATES];
 
 void setup() {
 
     Serial.begin(9600);
-
-    override1button.setDebounceTime(100); // set debounce time to 100 milliseconds
-    override2button.setDebounceTime(100); // set debounce time to 100 milliseconds
-    override3button.setDebounceTime(100); // set debounce time to 100 milliseconds
-
-    override1button.setCountMode(COUNT_FALLING);
-    override2button.setCountMode(COUNT_FALLING);
-    override3button.setCountMode(COUNT_FALLING);
-
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(PIN_PCA9865_ENABLE, OUTPUT);
+    digitalWrite(PIN_PCA9865_ENABLE, LOW);
 
     Serial.println("Servo ButterFly Diagnostic");
 
     servo_driver.begin();
     servo_driver.setOscillatorFrequency(27000000);
     servo_driver.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
-
-    gate_1.begin();
-    gate_2.begin();
-    gate_3.begin();
 
     delay(10);
 
@@ -105,65 +90,58 @@ void setup() {
 void loop() {
 
   printSystemState();
-  override1button.loop();
-  override2button.loop();
-  override3button.loop();
-
   checkGateSwitchState();
-
-  checkOverideButtons();
-
   controlGatesByArray();
-
-
-
-  setDustExtractor(areAnyGatesOpen());
 }
 
 void printSystemState(){
 
-  Serial.print("gate config: " );
-  Serial.println(gate_config_state);
-
-  Serial.print("gate 1 state: " );
-  Serial.println(gate_1.read());
-
   Serial.print("gates gate_states_array: " );
   Serial.print(gate_states_array[0]);
   Serial.print(gate_states_array[1]);
-  Serial.print(gate_states_array[2]);
+  Serial.println(gate_states_array[2]);
 
-  Serial.print("switch mode state: " );
-  Serial.println(mode.read());
+  Serial.print("buttons previous_button_states_array: " );
+  Serial.print(previous_button_states_array[0]);
+  Serial.print(previous_button_states_array[1]);
+  Serial.println(previous_button_states_array[2]);
+
+  Serial.print("buttons button_states_array: " );
+  Serial.print(button_states_array[0]);
+  Serial.print(button_states_array[1]);
+  Serial.println(button_states_array[2]);
 
   Serial.print("Extractor state: " );
   Serial.println(extractorState);
 
-
-
-
-  return;
-
 }
-
-
 
 void controlGate(int gateNumber, bool state){
 
-  Serial.print("control Gate number: ");
-  Serial.println(gateNumber);
-  Serial.print("control Gate state: ");
-  Serial.println(state);
+  int servo_number = gateNumber-1;
 
-  if(state){
-    servo_driver.setPWM(gateNumber, 0, SERVOMIN);
-  }else{
-    servo_driver.setPWM(gateNumber, 0, SERVOMAX);
+  if(previous_gate_states_array[servo_number]!=gate_states_array[servo_number]){
+
+
+    digitalWrite(PIN_PCA9865_ENABLE, HIGH);
+    delay(100);
+
+    if(state){
+      servo_driver.setPWM(servo_number, 0, SERVOMIN);
+    }else{
+      servo_driver.setPWM(servo_number, 0, SERVOMAX);
+    }
+    delay(100);
+
+    //servo_driver.setPWM(gateNumber-1, 0, 4096);
+    //servo_driver.setPWM(gateNumber, 0, 0);
+    digitalWrite(PIN_PCA9865_ENABLE, LOW);
+
+    previous_gate_states_array[servo_number]=state;
+
   }
-  delay(500);
-  //servo_driver.setPWM(gateNumber, 0, 0);
+  digitalWrite(PIN_PCA9865_ENABLE, LOW); // attempt to disable the servo driver to stop humming.
 
-  gate_states_array[gateNumber-1]=state;
 }
 
 void closeAllGatesArray(){
@@ -173,72 +151,51 @@ void closeAllGatesArray(){
 }
 
 void closeAllGates(){
-  controlGate(GATE_1,false);
-  controlGate(GATE_2,false);
-  controlGate(GATE_3,false);
+  controlGate(BUTTERFLY_GATE_1,false);
+  controlGate(BUTTERFLY_GATE_2,false);
+  controlGate(BUTTERFLY_GATE_3,false);
 
 }
 
 void controlGatesByArray(){
-  controlGate(GATE_1, gate_states_array[0]);
-  controlGate(GATE_2, gate_states_array[1]);
-  controlGate(GATE_3, gate_states_array[2]);
+  controlGate(BUTTERFLY_GATE_1, gate_states_array[0]);
+  controlGate(BUTTERFLY_GATE_2, gate_states_array[1]);
+  controlGate(BUTTERFLY_GATE_3, gate_states_array[2]);
 }
-
 
 void checkGateSwitchState(){
 
-  int lastGateOpened =0;
+  override_gate_1_button.poll();
+  override_gate_2_button.poll();
+  override_gate_3_button.poll();
 
-  if(gate_1.isReleased() && lastGateOpened!= 1){
-    gate_states_array[0] = true;
-    lastGateOpened =1;
+  blast_gate_1_button.poll();
+  blast_gate_2_button.poll();
+  blast_gate_3_button.poll();
 
-  }else{
+  button_states_array[0] =blast_gate_1_button.on() || override_gate_1_button.on();
+  button_states_array[1] =blast_gate_2_button.on() || override_gate_2_button.on();
+  button_states_array[2] =blast_gate_3_button.on() || override_gate_3_button.on();
+
+  if(!previous_button_states_array[0] && button_states_array[0]){
+      gate_states_array[0] = true;
+      gate_states_array[1] = false;
+      gate_states_array[2] = false;
+  }
+  if(!previous_button_states_array[1] && button_states_array[1]){
     gate_states_array[0] = false;
-  }
-
-
-  if(gate_2.isReleased() && lastGateOpened!= 2){
     gate_states_array[1] = true;
-    lastGateOpened =2;
-
-  }else{
-    gate_states_array[1] = false;
-  }
-
-  if(gate_3.isReleased() && lastGateOpened!= 3){
-    gate_states_array[2] = true;
-    lastGateOpened =3;
-
-  }else{
     gate_states_array[2] = false;
   }
-
-}
-
-void checkOverideButtons(){
-  if (override1button.getCount()){
-    bool swithTo = !gate_states_array[0]
-    closeAllGatesArray();
-    gate_states_array[0] = swithTo;
-
-    override1button.resetCount();
+  if(!previous_button_states_array[2] && button_states_array[2]){
+    gate_states_array[0] = false;
+    gate_states_array[1] = false;
+    gate_states_array[2] = true;
   }
-  if( override2button.getCount()){
-    bool swithTo = !gate_states_array[1]
-    closeAllGatesArray();
-    gate_states_array[1] = swithTo;
 
-    override2button.resetCount();
-
-  }
-  if( override3button.getCount()){
-    bool swithTo = !gate_states_array[2]
-    closeAllGatesArray();
-    gate_states_array[2] = swithTo;
-    override3button.resetCount();
-  }
+  previous_button_states_array[0] = button_states_array[0];
+  previous_button_states_array[1] = button_states_array[1];
+  previous_button_states_array[2] = button_states_array[2];
 }
 
 void setDustExtractor(bool state){
@@ -256,7 +213,6 @@ void setDustExtractor(bool state){
 
 }
 
-
 bool areAnyGatesOpen(){
   for(int i=0; i<NUM_GATES;i++){
     if(gate_states_array[i]){
@@ -266,4 +222,5 @@ bool areAnyGatesOpen(){
       return false;
     }
   }
+  return false;
 }
